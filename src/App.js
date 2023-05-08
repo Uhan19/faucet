3ffccbe3513b41796b1c541a0bc6a20646e9addf
Yuehan Duan <yuehanduan@yahoo.com>
2023-05-08 14:02:43 -0700

    Complete FE

--- src/App.js
+++ src/App.js
@@ -1,24 +1,75 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useEffect, useState } from "react";
+import Web3 from "web3";
+import detectEthereumProvider from "@metamask/detect-provider";
+import { loadContract } from "./utils/load-contract";
 
 function App() {
+  const [web3Api, setWeb3Api] = useState({
+    provider: null,
+    web3: null,
+    contract: null,
+  });
+
+  const [account, setAccount] = useState(null);
+
+  useEffect(() => {
+    const loadProvider = async () => {
+      const provider = await detectEthereumProvider();
+      const contract = await loadContract("Faucet", provider);
+      if (provider) {
+        setWeb3Api({
+          web3: new Web3(provider),
+          provider,
+          contract,
+        });
+      } else {
+        alert("Please install MetaMask!");
+      }
+    };
+    loadProvider();
+  }, []);
+
+  useEffect(() => {
+    const getAccounts = async () => {
+      const accounts = await web3Api.web3.eth.getAccounts();
+      setAccount(accounts[0]);
+    };
+
+    web3Api.web3 && getAccounts();
+  }, [web3Api.web3]);
+
+  const onClickHandler = async () => {
+    // loadProvider();
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="faucet-wrapper">
+        <div className="faucet">
+          <div className="is-flex is-flex-direction-row">
+            <span>
+              <strong>Account: </strong>
+            </span>
+            {account ? (
+              <span className="ml-2">{account}</span>
+            ) : (
+              <button
+                className="button is-small is-light ml-2"
+                onClick={onClickHandler}
+              >
+                Connect Wallet
+              </button>
+            )}
+          </div>
+          <div className="balance-view is-size-2 mb-4">
+            Current Balance: <strong>10</strong> ETH
+          </div>
+          <button className="button is-link mr-2">Donate</button>
+          <button className="button is-primary">Withdraw</button>
+        </div>
+      </div>
+    </>
   );
 }
 
